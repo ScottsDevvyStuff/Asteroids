1e419fcb8152358688be0c122c461ace75384bd1
Scott <scott.graham@oxbury.com>
2025-02-13 15:17:36 +0000

    implemented player rotation

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def main():
     clock = pygame.time.Clock()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)

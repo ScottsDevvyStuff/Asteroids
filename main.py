07d99c7fa8ff15beea69ff921645542ff6a6ea23
Scott <scott.graham@oxbury.com>
2025-02-13 16:55:21 +0000

    modified main to use groups for updatable and drawable objects to avoid calling each instance

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt = clock.tick(60)
-        dt = dt / 1000
+
+        # limits fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
